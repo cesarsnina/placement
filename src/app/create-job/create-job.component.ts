d5d9f1d70616d078a89f4a4021c937748becab4f
saliouprogress <msd228@cornell.edu>
2022-08-21 22:59:41 +0000

    made changes to create job page

--- src/app/create-job/create-job.component.ts
+++ src/app/create-job/create-job.component.ts
@@ -1,99 +1,123 @@
 import { Component, OnInit, Input } from '@angular/core';
-import {FormControl, Validators} from '@angular/forms';
+import { Router } from '@angular/router';
+import {
+  FormBuilder,
+  Validators,
+  FormArray,
+} from '@angular/forms';
 
 import { JobsService } from '../services/jobs.service';
-// import { JobsModel } from '../models/jobs.model';
+import { JobSkillsService } from '../services/job-skills.service'
+import { SkillsService } from '../services/skills.service'
 
-interface Animal {
-  name: string;
-  sound: string;
+import { JobsModel } from '../models/jobs.model';
+import { JobSkillsModel} from '../models/job-skills.model'
+import { SkillsModel} from '../models/skills.model'
+import { UsersModel } from '../models/users.model';
+
+interface Food {
+  value: string;
+  viewValue: string;
 }
-
-[
-  {
-      "id": 1,
-      "company": "Photosnap",
-      "logo": "./assets/images/photosnap.svg",
-      "new": true,
-      "featured": true,
-      "position": "Senior Frontend Developer",
-      "role": "Frontend",
-      "level": "Senior",
-      "postedAt": "1d ago",
-      "contract": "Full Time",
-      "location": "USA Only",
-      "createdAt": "2022-06-08T07:40:04.839Z",
-      "updatedAt": "2022-06-08T07:40:04.839Z",
-      "UserId": 5,
-      "Skills": [
-          {
-              "id": 13,
-              "name": "HTML",
-              "JobSkill": {
-                  "JobId": 1,
-                  "SkillId": 13
-              }
-          }
-      ]
-  },
-]
-export interface JobsModel {
-  id: number;
-  company: String;
-  logo: String;
-  new: boolean;
-  featured: boolean;
-  position: String;
-  role: String;
-  level: String;
-  postedAt: String;
-  contract: String;
-  location: String;
-  createdAt: string;
-  updatedAt: string;
-  UserId: number
-  Skills: {
-    id: number;
-    name: string;
-    JobSkill: {
-      JobId: number;
-      SkillId: number; };
-  }[];
-}
-
 
 @Component({
   selector: 'app-create-job',
   templateUrl: './create-job.component.html',
-  styleUrls: ['./create-job.component.css']
+  styleUrls: ['./create-job.component.css'],
 })
 export class CreateJobComponent implements OnInit {
-  @Input() jobs?: JobsModel[];
 
-  animalControl = new FormControl(null, Validators.required);
-  selectFormControl = new FormControl('', Validators.required);
-  animals: Animal[] = [
-    {name: 'Dog', sound: 'Woof!'},
-    {name: 'Cat', sound: 'Meow!'},
-    {name: 'Cow', sound: 'Moo!'},
-    {name: 'Fox', sound: 'Wa-pa-pa-pa-pa-pa-pow!'},
+  @Input() users?: UsersModel[];
+  @Input() jobSkill?: JobSkillsModel[];
+  @Input() skills?: SkillsModel[];
+
+  skillsList: Food[] = [
+    {value: 'steak-0', viewValue: 'Steak'},
+    {value: 'pizza-1', viewValue: 'Pizza'},
   ];
 
+  signUpForm = this.formBuilder.group({
+    company: ['', Validators.required],
+    logo: ['', Validators.required],
+    new: [false, Validators.required],
+    featured: [false, Validators.required],
+    position: ['', Validators.required],
+    role: ['', Validators.required],
+    level: ['', Validators.required],
+    postedAt: ['', Validators.required],
+    contract: ['', Validators.required],
+    location: ['', Validators.required],
+    UserId: [null, Validators.required],
+    skills: this.formBuilder.array([]),
+  });
+
   constructor(
-    private jobsService: JobsService 
-    ) {}
-  
-  retrieveJobs(): void {
-    this.jobsService.getAll()
+    private formBuilder: FormBuilder,
+    private jobsService: JobsService,
+    private jobSkillsService: JobSkillsService,
+    private skillsService: SkillsService,
+    private route: Router,
+  ) 
+  {}
+
+  onSubmit() {
+    this.signUpForm.get('postedAt')?.setValue("1dago");
+    this.signUpForm.get('UserId')?.setValue(1);
+    if(this.signUpForm.valid) {
+      this.addNewJob(this.signUpForm.value);
+      // console.table(this.signUpForm.value);
+    }
+  }
+
+  onAddSkills() {
+    const control = this.formBuilder.control('skill');
+    this.jobSkills.push(control);
+  }
+
+  get jobSkills() {
+    return this.signUpForm.get('skills') as FormArray;
+  }
+
+  addNewJob(job: JobsModel): void {
+      console.log(job)
+      this.jobsService.addJob(job).subscribe((response: JobsModel) => {
+        console.log("Jobs Model", response);
+      });
+  }
+
+  ngOnInit(): void {
+    this.signUpForm.valueChanges.subscribe((value) => {
+      // console.table(value);
+    }
+    );
+
+    this.jobSkillsService.getAll().subscribe({
+      next: (data) => {
+        this.jobSkill = data;
+        // console.log(this.jobSkill)
+      },
+      error: (e) => console.error(e)
+    });
+
+    this.skillsService.getAll()
       .subscribe({
         next: (data) => {
-          this.jobs = data;
+          this.skills = data;
+          // console.log(this.skills)
         },
         error: (e) => console.error(e)
       });
   }
-  ngOnInit(): void {
-    this.retrieveJobs();
-
-  }
 }
+
+// const newSkills = await Skill.findAll({raw: true});
+    // for (let i = 0; i < newSkills.length; i++) { 
+    //   if (request.body.skills.includes(newSkills[i].name)) {
+    //     const createSkill = await JobSkill.create({
+    //       JobId: request.body.id,
+    //       SkillId: newSkills[i].id
+    //     });
+    //     await createSkill.save()
+    //     console.log("newJob.id", newJob.id, createSkill)
+    //   }
+    // }
--- src/app/services/jobs.service.ts
+++ src/app/services/jobs.service.ts
@@ -1,24 +1,32 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { concat, Observable } from 'rxjs';
 import { JobsModel } from '../models/jobs.model';
 const getJobs = 'http://localhost:3005/jobs';
+const usersURL = 'http://localhost:3005/users';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-
 export class JobsService {
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
   getAll(): Observable<JobsModel[]> {
-    return this.http.get<JobsModel[]>(getJobs); 
+    return this.http.get<JobsModel[]>(getJobs);
   }
 
   jobs: JobsModel[];
   currentJobId: number = 1;
 
   getCurrentJob(jobId: number): Observable<JobsModel> {
-    const  getJob = getJobs.concat('/', jobId.toString());
+    const getJob = getJobs.concat('/', jobId.toString());
     return this.http.get<JobsModel>(getJob);
-}
+  }
+
+  addJob(job: JobsModel): Observable<JobsModel> {
+    const userId = (job.UserId).toString();
+    const action = "create-job";
+    const addJobURL = usersURL.concat("/",userId, "/",action);
+    return this.http.post<JobsModel>(addJobURL, job);
+  }
+
 
 }
