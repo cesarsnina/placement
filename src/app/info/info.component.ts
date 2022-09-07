6a05b419a2a2c7977b3122ce1177a97d41dd9ee3
saliouprogress <msd228@cornell.edu>
2022-09-07 17:28:57 +0000

    updated description page

--- src/app/info/info.component.ts
+++ src/app/info/info.component.ts
@@ -1,24 +1,29 @@
 import { Component, OnInit, Input } from '@angular/core';
-import {Router} from '@angular/router';
+import { Router } from '@angular/router';
 import { JobsModel } from '../models/jobs.model';
 import { UsersService } from '../services/users.service';
 import { JobsService } from '../services/jobs.service';
+import { StateService } from '../services/state.service';
 
 @Component({
   selector: 'app-info',
   templateUrl: './info.component.html',
-  styleUrls: ['./info.component.css']
+  styleUrls: ['./info.component.css'],
 })
 export class InfoComponent implements OnInit {
   @Input() item: JobsModel;
 
-  constructor(private route:Router, private userService: UsersService, private jobsService: JobsService) { }
+  constructor(
+    private route: Router,
+    private userService: UsersService,
+    private jobsService: JobsService,
+    private stateService: StateService
+  ) {}
 
   navigateToJob() {
     this.jobsService.currentJobId = this.item.id;
-    setTimeout(() => {
-      this.route.navigate(['/job-description']);
-    }, 1000)
+    this.stateService.state.currentJob.jobId = this.item.id;
+    this.route.navigate(['/job-description']);
   }
 
   ngOnInit(): void {}
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { UsersModel } from '../models/users.model';
 import { UsersService } from '../services/users.service';
+import { StateService } from '../services/state.service';
 
 @Component({
   selector: 'app-login',
@@ -17,7 +18,11 @@ export class LoginComponent implements OnInit {
     password: new FormControl(''),
   });
 
-  constructor(private usersService: UsersService, private route: Router) {}
+  constructor(
+    private usersService: UsersService,
+    private route: Router,
+    private stateService: StateService
+  ) {}
 
   onSubmit() {
     const user = this.logInForm.value;
@@ -27,22 +32,35 @@ export class LoginComponent implements OnInit {
   logUser(user: UsersModel[]): void {
     this.usersService.logUser(user).subscribe({
       next: (data) => {
-        console.log("response", data);
+        this.retrieveUsers();
+        this.route.navigate(['/']);
       },
       error: (e) => console.error(e),
     });
-    
   }
 
   retrieveUsers(): void {
     this.usersService.getAll().subscribe({
       next: (data) => {
         this.users = data;
+        const lastIndex = this.users?.length - 1;
+        this.stateService.state = {
+          currentUser: {
+            firstName: this.users[lastIndex].firstName,
+            lastName: this.users[lastIndex].lastName,
+            email: this.users[lastIndex].email,
+            userId: this.users[lastIndex].id,
+            isLoggedIn: true,
+          },
+          currentJob: {
+            jobId: 0,
+          },
+        };
       },
       error: (e) => console.error(e),
     });
   }
   ngOnInit(): void {
-    this.retrieveUsers();
+    // this.retrieveUsers();
   }
 }
